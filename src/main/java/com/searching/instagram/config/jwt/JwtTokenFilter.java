bb346fb86fb95ef70c6fe63a7783955c8563783a
Searching <ssirojiddin2004@gmail.com>
2022-04-07 15:57:34 +0500

    Add Access-Control settings to JwtTokenFilter

--- src/main/java/com/searching/instagram/config/jwt/JwtTokenFilter.java
+++ src/main/java/com/searching/instagram/config/jwt/JwtTokenFilter.java
@@ -64,6 +64,12 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
         filterChain.doFilter(request, response);
     }
 }
